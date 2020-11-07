eb4b7f35cc6e31d5dd1741163ea23fdca66cd48f
Chen Yijun <olivergtarrow24@gmail.com>
2020-11-07 02:28:18 -0500

    File Upload && Library System created

--- picsmagic/src/components/HomeComponent.js
+++ picsmagic/src/components/HomeComponent.js
@@ -1,6 +1,6 @@
-import React, { Component} from "react";
+import React, { Component, useCallback} from "react";
 import ImageEditor from "@toast-ui/react-image-editor";
-import {Button} from "reactstrap";
+import {Button,Modal,ModalHeader,ModalBody,ModalFooter,Card,CardBody,CardTitle} from "reactstrap";
 const icona = require("tui-image-editor/dist/svg/icon-a.svg");
 const iconb = require("tui-image-editor/dist/svg/icon-b.svg");
 const iconc = require("tui-image-editor/dist/svg/icon-c.svg");
@@ -21,12 +21,35 @@ class Home extends Component {
   constructor(props){
     super(props);
     this.state={
-        imageSrc: "images/mcmaster.jpg"
+        isModalOpen:false,
+        imageSelected:this.props.imageSelected,
+        imageLib : this.props.imageLib,
+        curId:this.props.curId
     }
-    this.handleClickButton = this.handleClickButton.bind(this);
     this.saveImageToDisk = this.saveImageToDisk.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.confirmModal = this.confirmModal.bind(this);
+    this.checkhighlight = this.checkhighlight.bind(this);
+    this.uploadHandleClick = this.uploadHandleClick.bind(this);
+    this.uploadHandleChange = this.uploadHandleChange.bind(this);
   }
   imageEditor = React.createRef();
+  //handle upload functions
+  hiddenFileInput = React.createRef();
+
+  toggleModal(){
+      this.setState({
+        isModalOpen :!this.state.isModalOpen
+      });
+  }
+  confirmModal(){
+    const editorInstance = this.imageEditor.current.getInstance();
+    editorInstance.loadImageFromURL(this.props.imageSelected.name,"image");
+    this.props.changeImageSelected(this.state.imageSelected);
+    this.props.changeImageLib(this.state.imageLib);
+    this.props.changeCurId(this.state.curId);
+    this.toggleModal();
+    }
   
   //Saving method. Will be used later
   saveImageToDisk (){
@@ -38,21 +61,51 @@ class Home extends Component {
       download(data, `image.${extension}`, mimeType);
     }
   };
-  handleClickButton () {
-    const editorInstance = this.imageEditor.current.getInstance();
-    console.log(editorInstance);
-    editorInstance.flipX();
-  };
   componentDidMount(){
     this.imageEditor.current.getRootElement().children[0].classList.add('d-none');
     this.imageEditor.current.getRootElement().children[1].children[0].classList.add('d-none');
   }
+  //check highlighted picture in upload 
+  checkhighlight(img){
+    const check = this.state.imageSelected.id===img.id? "highlight":"";
+    return check;
+  }
+  
+  /* Programatically click the hidden file input element
+   when the Button component is clicked*/
+  uploadHandleClick (){
+    this.hiddenFileInput.current.click();
+  };
+  /*Call a function (passed as a prop from the parent component)
+   to handle the user-selected file */
+  uploadHandleChange (event) {
+    const fileUploaded = URL.createObjectURL(event.target.files[0]);
+    const newImage = {
+        name: fileUploaded,
+        id:this.state.curId+1
+      }
+      this.setState({
+          curId:this.state.curId+1,
+          imageLib: [...this.state.imageLib, newImage]
+      });
+  };
+
   render(){
+    const imgLibrary= this.state.imageLib.map((image)=>{
+        return(
+            <div key={image.id} className="col-lg-4 col-6">
+                <img alt={image.name} 
+                className={"img-thumbnail " + this.checkhighlight(image)}
+                onClick={()=>this.setState({imageSelected:image})}
+                src={image.name} />
+            </div>
+            );
+        });
     return (
         <div className="row row-content">
-            <div className="col-md-9 col-12  order-md-1 canvas">
+            <div className="col-md-8 col-12  order-md-1 canvas">
                 <div className="row justify-content-center mb-4">
-                    <Button className="mr-3" onClick={this.handleClickButton} color="primary">Basics</Button>{' '}
+                    <Button className="mr-3" color="primary">Basics</Button>{' '}
                     <Button className="mr-3" color="secondary">Filter</Button>{' '}
                     <Button className="mr-3" color="success">Crop</Button>{' '}
                     <Button className="mr-3" color="info">Draw</Button>{' '}
@@ -65,7 +118,7 @@ class Home extends Component {
                         ref={this.imageEditor}
                         includeUI={{
                             loadImage: {
-                            path: this.state.imageSrc,
+                            path: this.state.imageSelected.name,
                             name: "image",
                             },
                             theme: myTheme,
@@ -89,9 +142,39 @@ class Home extends Component {
             <div className="col-md-2 col-12 order-md-0">
             
             </div> 
-            {/* This column is for the Output button on the right */}
-            <div className="col-md col-12 order-md-2">
-
+            {/* This column is for the Output button and upload on the right */}
+            <div className="col-md-2 col-12 order-md-2 ">
+                <div className="row justify-content-center">
+                    <Button className="btn-outline-primary" onClick={this.toggleModal}>Load</Button>{' '}
+                    <Modal className="modal-lg" isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                            <ModalHeader toggle={this.toggleModal}>Select a Picture</ModalHeader>
+                            <ModalBody>
+                                <div className="row">
+                                    <div key="0"className="col-lg-4 col-6 mb-1">
+                                        <Card>
+                                            <CardBody className="text-center"> 
+                                                <CardTitle alt="Upload"
+                                                onClick={this.uploadHandleClick}
+                                                ><i className="fa fa-3x fa-upload"></i><br/>
+                                                <a href="#">Upload</a> 
+                                                </CardTitle>
+                                            </CardBody>
+                                        </Card>
+                                        <input type="file" accept="image/*" id="input-image-file" 
+                                        ref={this.hiddenFileInput}
+                                        onChange={this.uploadHandleChange}
+                                        style={{display: 'none'}} 
+                                        />
+                                    </div>
+                                    {imgLibrary}
+                                </div>
+                            </ModalBody>
+                            <ModalFooter>
+                            <Button color="primary" onClick={this.confirmModal}>Confirm</Button>{' '}
+                            <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                            </ModalFooter>
+                        </Modal>
+                </div>
             </div>
             {/*
         <ImageEditor
--- picsmagic/src/components/MainComponent.js
+++ picsmagic/src/components/MainComponent.js
@@ -6,18 +6,43 @@ import HelpCenter from './HelpCenter';
 import {Switch, Route,Redirect} from 'react-router-dom';
 import { createBrowserHistory as createHistory } from "history";
 const history = createHistory();
+const images=
+    [
+    {name:"images/mcmaster0.jpg", id:1},
+    {name:"images/mcmaster1.jpg", id:2},
+    {name:"images/mcmaster2.jpg", id:3},
+    {name:"images/mcmaster3.jpg", id:4},
+    {name:"images/mcmaster4.jpg", id:5},
+    {name:"images/mcmaster5.jpg", id:6},
+    ]
 
 class Main extends Component {
   constructor(props){
     super(props);
+    this.state={
+      imageLib:images,
+      imageSelected:{name:"images/mcmaster0.jpg",id:"1"},
+      curId:6
+    }
   }
-
   render() {
+    const HomePage=(props)=>{
+      return(
+        <Home {...props} 
+        imageLib={this.state.imageLib} 
+        imageSelected={this.state.imageSelected}
+        curId={this.state.curId}
+        changeImageSelected={(value)=>this.setState({imageSelected:value})} 
+        changeImageLib={(value)=>this.setState({imageLib:value})}
+        changeCurId={(value)=>this.setState({curId:value})} 
+        />
+      );
+    }
     return (
       <div >
           <Header/>
             <Switch history={history}>
-              <Route path="/home" exact component={Home} />
+              <Route path="/home" exact component={HomePage} />
               <Route path="/helpcenter" component={HelpCenter} />
               <Redirect to="/home"></Redirect>
             </Switch>
