383ed7b75cf1cde2323568876e3c1fd04b89425a
Chen Yijun <olivergtarrow24@gmail.com>
2020-11-08 20:01:50 -0500

    Header Logo added

--- picsmagic/src/components/DrawComponent.js
+++ picsmagic/src/components/DrawComponent.js
@@ -1,33 +1,56 @@
-import React from 'react';
-import {Button, Card, CardBody,CardTitle,CardText} from "reactstrap";
+import React,{useState} from 'react';
+import reactCSS from 'reactcss';
+import {Button, Card, CardBody,CardTitle,CardText,ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem} from "reactstrap";
+import {TwitterPicker} from 'react-color';
 
 function Draw(props){
+    const [dropdownOpen, setOpen] = useState(false);
+    const [color, setColor] = useState({
+        r: '241',
+        g: '112',
+        b: '19',
+        a: '1'
+    })
+    const toggle = () => setOpen(!dropdownOpen);
+
+    const styles = reactCSS({
+    'default': {
+        color: {
+        background: `rgba(${ color.r }, ${ color.g }, ${ color.b }, ${ color.a })`,
+        },
+        popover: {
+        position: 'absolute',
+        zIndex: '2',
+        },
+        cover: {
+        position: 'fixed',
+        top: '0px',
+        right: '0px',
+        bottom: '0px',
+        left: '0px',
+        },
+    },
+    });
+    const colorChange= (color)=> setColor(color.rgb);
+
     return (
-        <div className="container">
-            <div className="row">
-                <Card>
-                    <CardBody>
-                        <CardTitle tag="h5">
-                            Settings
-                        </CardTitle>
-                        <CardText>
-                            Color Ragge
-                        </CardText>
-                    </CardBody>
-                </Card>
-            </div>
-            <div className="row">
-                <Card>
-                    <CardBody>
-                        <CardTitle tag="h5">
-                            Settings
-                        </CardTitle>
-                        <CardText>
-                            Color Ragge
-                        </CardText>
-                    </CardBody>
-                </Card>
-            </div>
+        <div className="row row-bar-content">
+            <Card>
+                <CardBody>
+                    <CardTitle tag="h6">
+                        Settings:
+                    </CardTitle>
+                    <CardText>
+                        Color:{' '}
+                        <button onClick={toggle}  className="btn " style={styles.color}>{' '}</button>
+                            { dropdownOpen ? <div style={ styles.popover }>
+                            <div style={ styles.cover } onClick={ toggle }/>
+                            <TwitterPicker onChange={colorChange}/>
+                            </div> : null }
+
+                    </CardText>
+                </CardBody>
+            </Card>
         </div>
     );
 }
