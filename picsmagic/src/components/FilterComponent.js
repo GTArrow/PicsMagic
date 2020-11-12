69f6e5f40a7b13139d4a16aaaf362d95df38e1f1
Ace Huang <Ace@Aces-mac.local>
2020-11-12 17:02:31 -0500

    change filter panel layout

--- picsmagic/src/components/FilterComponent.js
+++ picsmagic/src/components/FilterComponent.js
@@ -9,7 +9,7 @@ function Filter(props){
                             <CardText>
                             <Button outline colour='light' active={props.grayscaleselected}> 
                                     <Media left >
-                                        <Media object src="/images/filter/grayscale.png" width='100' alt="grayscale" onClick={()=>props.GrayscaleFilter()}/>
+                                        <Media object src="/images/filter/grayscale.png" width='150' alt="grayscale" onClick={()=>props.GrayscaleFilter()}/>
                                     </Media>
                                     <Media body className="mt-auto mb-auto">
                                         GrayScale
@@ -19,7 +19,7 @@ function Filter(props){
                             <CardText>
                             <Button outline colour='light' active={props.sepiaselected}> 
                                     <Media left>
-                                        <Media object src="/images/filter/sepia.png" width='100' alt="grayscale" onClick={()=>props.SepiaFilter()}/>
+                                        <Media object src="/images/filter/sepia.png" width='150' alt="grayscale" onClick={()=>props.SepiaFilter()}/>
                                     </Media>
                                     <Media body className="mt-auto mb-auto">
                                     Sepia
@@ -29,7 +29,7 @@ function Filter(props){
                             <CardText>
                             <Button outline colour='light' active={props.embossselected}> 
                                     <Media left>
-                                        <Media object src="/images/filter/emboss.png" width='100' alt="grayscale" onClick={()=>props.EmbossFilter()}/>
+                                        <Media object src="/images/filter/emboss.png" width='150' alt="grayscale" onClick={()=>props.EmbossFilter()}/>
                                     </Media>
                                     <Media body className="mt-auto mb-auto">
                                     Emboss
@@ -39,7 +39,7 @@ function Filter(props){
                             <CardText>
                             <Button outline colour='light' active={props.invertselected}>
                                     <Media left>
-                                        <Media object src="/images/filter/invert.png" width='100' alt="grayscale" onClick={()=>props.InvertFilter()}/>
+                                        <Media object src="/images/filter/invert.png" width='150' alt="grayscale" onClick={()=>props.InvertFilter()}/>
                                     </Media>
                                     <Media body className="mt-auto mb-auto">
                                     Invert
@@ -49,7 +49,7 @@ function Filter(props){
                             <CardText>
                             <Button outline colour='light' active={props.sharpenselected}> 
                                     <Media left>
-                                        <Media object src="/images/filter/sharpen.png" width='100' alt="grayscale" onClick={()=>props.SharpenFilter()}/>
+                                        <Media object src="/images/filter/sharpen.png" width='150' alt="grayscale" onClick={()=>props.SharpenFilter()}/>
                                     </Media>
                                     <Media body className="mt-auto mb-auto">
                                     Sharpen
